a6f72ad6d33081daa7ab35906f3e707013d4a0c8
andymil <andrew.j.milson@gmail.com>
2014-07-09 10:12:18 +1200

    added concatenating and compressing of css. Also updated gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -4,27 +4,25 @@ module.exports = function (grunt) {
     my_project: {
       src: 'source',
       assets: 'assets',
-      css: ['<%= my_project.src %>/css/style.styl', '<%= my_project.src %>/css/layout.styl'],
-      js: ['<%= my_project.src %>/js/*.js']
-    },
-    my_tag: {
-      banner: '/*!\n' +
+      stylus: ['<%= my_project.src %>/css/style.styl', '<%= my_project.src %>/css/layout.styl'],
+      css: '<%= my_project.assets %>/css/**/*.css',
+      css_banner: '/*!\n' +
         ' * <%= pkg.author %>\n' +
         ' * <%= pkg.title %>\n' +
         ' * <%= pkg.url %>\n' +
         ' * @author <%= pkg.author %>\n' +
         ' * @version <%= pkg.version %>\n' +
         ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
-        ' */\n'
+        ' */\n',
+      js: ['<%= my_project.src %>/js/*.js']
     },
     stylus: {
       compile: {
         options: {
-          compress: false,
-          banner: '<%= my_tag.banner %>'
+          compress: false
         },
         files: {
-          '<%= my_project.assets %>/css/style.min.css': '<%= my_project.css %>'
+          '<%= my_project.assets %>/css/style.min.css': '<%= my_project.stylus %>'
         }
       }
     },
@@ -41,6 +39,16 @@ module.exports = function (grunt) {
         }
       }
     },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '<%= my_project.css_banner %>'
+        },
+        files: {
+          '<%= my_project.assets %>/css/main.min.css': ['<%= my_project.css %>']
+        }
+      }
+    },
     exec: {
       serve: {
         cmd: 'jekyll serve --watch'
@@ -49,7 +57,7 @@ module.exports = function (grunt) {
     watch: {
       stylus: {
         files: '<%= my_project.src %>/css/**/*.styl',
-        tasks: ['stylus']
+        tasks: ['stylus', 'cssmin']
       },
       livereload: {
         options: {
@@ -58,13 +66,15 @@ module.exports = function (grunt) {
         files: [
           '_config.yml',
           '**/*.{html,markdown}',
-          '<%= project.assets %>/js/**/*.js',
-          '<%= project.assets %>/**/*.{png,jpg,jpeg,gif,webp,svg,css}'
+          '<%= my_project.assets %>/js/**/*.js',
+          '<%= my_project.assets %>/**/*.{png,jpg,jpeg,gif,webp,svg,css}'
         ]
       }
     }
   });
 
+  grunt.registerTask("build", ["stylus"])
+
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-  grunt.registerTask('default', ['stylus', 'concurrent:target1', 'open']);
+  grunt.registerTask('default', ['stylus', 'cssmin', 'concurrent:target1', 'open']);
 };
