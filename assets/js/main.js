a88defb7636b0461dcac9104e6fd12d13d549453
Andrew Milson <andrew.j.milson@gmail.com>
2014-09-19 22:45:22 +1200

    Fixed minor issues.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,16 @@
 (function() {
   var profilePictures = document.getElementsByClassName("profile"),
-      sexyBeast = document.getElementById("sexy-beast"),
-      sexyBeastWink
-      frames = 10,
       maxScrollTop = 30;
 
-  console.log(profilePictures);
-
   window.onscroll = function() {
-    // $sexyBeast.src = "images/sexy-beast.jpg";
-    // $sexyBeast.style.webkitTransform = "scale(" +
-    //   Math.max(Math.min(1 + 3.5 / maxScrollTop * window.pageYOffset, 4.5), 1) + ") rotate(" +
-    //   Math.max(Math.min(20 / maxScrollTop * window.pageYOffset, 20), 0) + "deg)";
-    // $sexyBeast.style.left = Math.max(Math.min(140 / maxScrollTop * window.pageYOffset, 140), 0) + "px";
-    // $sexyBeast.style.top = Math.max(Math.min(190 / maxScrollTop * window.pageYOffset, 190), 0) + "px";
-    //
-    // if (window.pageYOffset > maxScrollTop) {
-    //   $sexyBeast.src = "images/sexy-beast-2.jpg";
-    // }
-
     [].forEach.call(profilePictures, function(profilePicture) {
-      profilePicture.style.webkitTransform = profilePicture.style.mozTransform = profilePicture.style.oTransform = profilePicture.style.msTransform =
-        'scale(' + Math.max(Math.min(1 + 3.5 / maxScrollTop * window.pageYOffset, 4.5), 1) + ') ' +
-        'rotate(' + Math.max(Math.min(20 / maxScrollTop * window.pageYOffset, 20), 0) + 'deg)';
-      profilePicture.style.left = Math.max(Math.min(140 / maxScrollTop * window.pageYOffset, 140), 0) + "px";
-      profilePicture.style.top = Math.max(Math.min(190 / maxScrollTop * window.pageYOffset, 190), 0) + "px";
+      profilePicture.style.webkitTransform = profilePicture.style.MozTransform = profilePicture.style.OTransform = profilePicture.style.msTransform =
+        'scale(' + Math.max(Math.min((1 + 3.5 / maxScrollTop * window.pageYOffset).toFixed(2), 4.5), 1) + ') ' +
+        'rotate(' + Math.max(Math.min((20 / maxScrollTop * window.pageYOffset).toFixed(1), 20), 0) + 'deg)';
+      profilePicture.style.left = Math.max(Math.min((140 / maxScrollTop * window.pageYOffset).toFixed(0), 140), 0) + "px";
+      profilePicture.style.top = Math.max(Math.min((190 / maxScrollTop * window.pageYOffset).toFixed(0), 190), 0) + "px";
       profilePicture.style.display = "block";
-
-      if (window.pageYOffset > 30 && profilePicture.id == "gazing") {
-        profilePicture.style.display = "none";
-      }
+      if (window.pageYOffset > maxScrollTop && profilePicture.id == "gazing") profilePicture.style.display = "none";
     });
   };
 })();
