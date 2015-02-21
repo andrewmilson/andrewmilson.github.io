699a621d29720681b5ef7b2ab9d454fd6ef5c463
Andrew Milson <andrew.j.milson@gmail.com>
2015-02-22 10:22:38 +1300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@
   window.onscroll = function() {
     [].forEach.call(profilePictures, function(profilePicture) {
       profilePicture.style.webkitTransform = profilePicture.style.MozTransform = profilePicture.style.OTransform = profilePicture.style.msTransform =
-        'scale(' + Math.max(Math.min((1 + 3.5 / maxScrollTop * window.pageYOffset).toFixed(2), 4.5), 1) + ') ' +
-        'rotate(' + Math.max(Math.min((20 / maxScrollTop * window.pageYOffset).toFixed(1), 20), 0) + 'deg)';
+        'scale(' + Math.max(Math.min((1 + 3.5 / maxScrollTop * window.pageYOffset).toFixed(4), 4.5), 1) + ') ' +
+        'rotate(' + Math.max(Math.min((20 / maxScrollTop * window.pageYOffset).toFixed(4), 20), 0) + 'deg)';
       profilePicture.style.left = Math.max(Math.min((140 / maxScrollTop * window.pageYOffset).toFixed(0), 140), 0) + "px";
       profilePicture.style.top = Math.max(Math.min((190 / maxScrollTop * window.pageYOffset).toFixed(0), 190), 0) + "px";
       profilePicture.style.display = "block";
